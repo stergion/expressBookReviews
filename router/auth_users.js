0b2f633e32156e139cc6079cf86bb13dcb7f9278
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2024-06-24 21:14:47 +0300

    Implement book review endpoint

--- router/auth_users.js
+++ router/auth_users.js
@@ -58,8 +58,21 @@ regd_users.post("/login", (req, res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let book = books[req.params.isbn];
+    let review = req.query.review;
+
+    if (!book) {
+        return res.status(404).json({ message: "Book not found." });
+    }
+
+    if (!review) {
+        return res.status(400).json({ message: "Review is missing." });
+    }
+
+    book.reviews[req.session.authorization.username] = review;
+
+    return res.status(200).json({ message: "Review added successfully." });
+    
 });
 
 module.exports.authenticated = regd_users;
