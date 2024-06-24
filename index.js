85393263463ceefafe910cc52ec9ebe05e69d3e1
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2024-06-24 18:50:35 +0300

    chore: Update session secret for customer authentication

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+require('dotenv').config();
+
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
+
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }));
+app.use("/customer", session({
+    secret: process.env.AUTH_SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
 
 app.use("/customer/auth/*", function auth(req, res, next) {
     //Write the authenication mechanism here
